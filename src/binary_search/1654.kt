07712a7dd070e1e6f396a18c65718c38e5f9085c
dpcks0509 <dpcks0509@gmail.com>
2024-07-12 13:06:10 +0900

    feat: 이분 탐색 - [Silver 2] 1654. 랜선 자르기

---
+++ src/binary_search/1654.kt
@@ -0,0 +1,37 @@
+package binary_search
+
+class `1654` {
+    fun solution(): String =
+        with(System.`in`.bufferedReader()) {
+            var answer = 0L
+
+            val (k, n) = readLine().split(" ").map { it.toInt() }
+            val numbers = LongArray(k) { readLine().toLong() }
+
+            var start = 1L
+            var end = numbers.max()
+
+            while (start <= end) {
+                val mid = (start + end) / 2
+                val sum = numbers.sumOf { it / mid }
+
+                if (sum >= n) {
+                    start = mid + 1
+                    answer = mid
+                } else {
+                    end = mid - 1
+                }
+            }
+
+            close()
+            return answer.toString()
+        }
+}
+
+fun main(): Unit =
+    with(System.out.bufferedWriter()) {
+        write(`1654`().solution())
+
+        flush()
+        close()
+    }
