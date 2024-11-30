b390b7d4c52da1174a3f0b5b9f6aca13397fd100
dpcks0509 <dpcks0509@gmail.com>
2024-12-01 03:10:21 +0900

    feat: greedy - [Silver 1] 1931. 회의실 배정

---
+++ src/tony/greedy/1931.kt
@@ -0,0 +1,51 @@
+package tony.greedy
+
+data class Time(
+    val start: Int,
+    val end: Int,
+)
+
+class `1931` {
+    fun solution(): String =
+        with(System.`in`.bufferedReader()) {
+            val answer = StringBuilder()
+
+            val n = readLine().toInt()
+            val times =
+                Array(n) {
+                    val (start, end) = readLine().split(' ').map { it.toInt() }
+                    Time(start, end)
+                }
+
+            times.sortWith { t1, t2 ->
+                if (t1.end == t2.end) {
+                    t1.start - t2.start
+                } else {
+                    t1.end - t2.end
+                }
+            }
+
+            var cnt = 0
+            var currentEnd = 0
+
+            times.forEach {
+                if (currentEnd <= it.start) {
+                    currentEnd = it.end
+                    cnt++
+                }
+            }
+
+            answer.append("$cnt")
+
+            close()
+            return answer.toString()
+        }
+}
+
+fun main(): Unit =
+    with(System.out.bufferedWriter()) {
+        write(`1931`().solution())
+
+        flush()
+        close()
+    }
