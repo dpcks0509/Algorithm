3640b19e1b953262d5cedd425f73b3acf0abee31
dpcks0509 <dpcks0509@gmail.com>
2024-12-02 17:36:59 +0900

    feat: math - [Silver 3] 2108. 통계학

---
+++ src/tony/math/2108.kt
@@ -0,0 +1,40 @@
+package tony.math
+
+import kotlin.math.roundToInt
+
+class `2108` {
+    fun solution(): String =
+        with(System.`in`.bufferedReader()) {
+            val answer = StringBuilder()
+
+            val n = readLine().toInt()
+            val numbers =
+                List(n) {
+                    readLine().toInt()
+                }
+
+            answer.appendLine("${(numbers.sum().toDouble() / n).roundToInt()}")
+
+            val sortedNumbers = numbers.sorted()
+            answer.appendLine("${(sortedNumbers[n / 2])}")
+
+            val freqMap = numbers.groupingBy { it }.eachCount()
+            val maxFreq = freqMap.maxOf{it.value}
+            val maxFreqNumbers = freqMap.filter { it.value == maxFreq }.keys.sorted()
+            val freq = if(maxFreqNumbers.size == 1) maxFreqNumbers[0] else maxFreqNumbers[1]
+            answer.appendLine("$freq")
+
+            answer.appendLine("${sortedNumbers.last() - sortedNumbers.first()}")
+
+            close()
+            return answer.toString()
+        }
+}
+
+fun main(): Unit =
+    with(System.out.bufferedWriter()) {
+        write(`2108`().solution())
+
+        flush()
+        close()
+    }
