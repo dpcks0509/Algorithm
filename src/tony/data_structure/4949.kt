12e9f7752d17960a0391745958b346fe3af36147
dpcks0509 <dpcks0509@gmail.com>
2024-12-02 13:44:02 +0900

    feat: data_structure - [Silver 4] 4949. 균형잡힌 세상

---
+++ src/tony/data_structure/4949.kt
@@ -0,0 +1,47 @@
+package tony.data_structure
+
+class `4949` {
+    fun solution(): String =
+        with(System.`in`.bufferedReader()) {
+            val answer = StringBuilder()
+
+            while (true) {
+                val str = readLine().toCharArray()
+                if (str.size == 1 && str[0] == '.') break
+
+                val deque = ArrayDeque<Char>()
+                var balance = "yes"
+                str.forEach {
+                    when (it) {
+                        '(', '[' -> deque.add(it)
+                        ')' -> {
+                            if (deque.isEmpty() || deque.removeLast() != '(') {
+                                balance = "no"
+                                return@forEach
+                            }
+                        }
+
+                        ']' -> {
+                            if (deque.isEmpty() || deque.removeLast() != '[') {
+                                balance = "no"
+                                return@forEach
+                            }
+                        }
+                    }
+                }
+                if(deque.isNotEmpty()) balance = "no"
+                answer.appendLine(balance)
+            }
+
+            close()
+            return answer.toString()
+        }
+}
+
+fun main(): Unit =
+    with(System.out.bufferedWriter()) {
+        write(`4949`().solution())
+
+        flush()
+        close()
+    }
