51c148e876921c2c3458c4a128e36cad421b7b7d
dpcks0509 <dpcks0509@gmail.com>
2024-12-03 20:49:29 +0900

    feat: dp - [Silver 3] 9095. 1, 2, 3 더하기

---
+++ src/tony/dp/9095.kt
@@ -0,0 +1,34 @@
+package tony.dp
+
+class `9095` {
+    fun solution(): String =
+        with(System.`in`.bufferedReader()) {
+            val answer = StringBuilder()
+
+            val t = readLine().toInt()
+            val numbers = List(t) { readLine().toInt() }
+
+            fun dp(n: Int): Int =
+                when (n) {
+                    1 -> 1
+                    2 -> 2
+                    3 -> 4
+                    else -> dp(n - 1) + dp(n - 2) + dp(n - 3)
+                }
+
+            numbers.forEach { n ->
+                answer.appendLine("${dp(n)}")
+            }
+
+            close()
+            return answer.toString()
+        }
+}
+
+fun main(): Unit =
+    with(System.out.bufferedWriter()) {
+        write(`9095`().solution())
+
+        flush()
+        close()
+    }
