ac4b61ae0b370d24e180944d7136ff39e688a248
dpcks0509 <dpcks0509@gmail.com>
2024-12-16 04:58:54 +0900

    feat: shortest_path - [Gold 5] 1916. 최소비용 구하기

--- src/baekjoon/shortest_path/1916.kt
+++ src/baekjoon/shortest_path/1916.kt
@@ -18,16 +18,22 @@ fun main() {
     val (origin, destination) = br.readLine().split(' ').map { it.toInt() }
 
     val costs = Array(n + 1) { Int.MAX_VALUE }
+    val visited = BooleanArray(n + 1)
     val priorityQueue = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
 
     costs[origin] = 0
+    visited[origin] = true
     priorityQueue.add(Pair(origin, 0))
 
     while (priorityQueue.isNotEmpty()) {
         val (currentNode, currentCost) = priorityQueue.remove()
-        if (currentCost >= costs[destination]) break
+        visited[currentNode] = true
+        if (currentNode == destination) break
 
-        graph[currentNode].forEach { (nextNode, nextCost) ->
+        for (i in 0 until graph[currentNode].size) {
+            val (nextNode, nextCost) = graph[currentNode][i]
+            if (visited[nextNode]) continue
+
             val sumCost = currentCost + nextCost
 
             if (sumCost < costs[nextNode]) {
