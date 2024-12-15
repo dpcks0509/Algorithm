a892468b3942880525c5e23395ef4c6684f6a9f2
dpcks0509 <dpcks0509@gmail.com>
2024-12-16 05:42:20 +0900

    feat: graph - [Silver 1] 11403. 경로 찾기

--- src/baekjoon/graph/11403.kt
+++ src/baekjoon/graph/11403.kt
@@ -2,38 +2,51 @@ package baekjoon.graph
 
 fun main() {
     val n = readln().toInt()
-    val graph = Array(n) { readln().split(" ").map { it.toInt() } }
+    val graph = Array(n) { readln().split(" ").map { it.toInt() }.toIntArray() }
 
-    val answer = Array(n) { IntArray(n) }
-
-    for (i in 0 until n) {
-        val queue = ArrayDeque<Int>()
-        for (j in 0 until n) {
-            val visited = BooleanArray(n)
-
-            if (graph[i][j] == 1) {
-                queue.add(j)
-                answer[i][j] = 1
-                visited[j] = true
-
-                while (queue.isNotEmpty()) {
-                    val v = queue.removeFirst()
-
-                    for (k in 0 until n) {
-                        if (graph[v][k] == 1) {
-                            if (visited[k]) continue
-
-                            queue.add(k)
-                            answer[i][k] = 1
-                            visited[k] = true
-                        }
-                    }
-                }
+    // floyd-warshall
+    for(k in 0 until n) {
+        for(i in 0 until n) {
+            for(j in 0 until n) {
+                if(graph[i][k] == 1 && graph[k][j] == 1) graph[i][j] = 1
             }
         }
     }
 
-    answer.forEach {
+    graph.forEach {
         println(it.joinToString(" "))
     }
+
+    // bfs
+//    val answer = Array(n) { IntArray(n) }
+//    for (i in 0 until n) {
+//        val queue = ArrayDeque<Int>()
+//        for (j in 0 until n) {
+//            val visited = BooleanArray(n)
+//
+//            if (graph[i][j] == 1) {
+//                queue.add(j)
+//                answer[i][j] = 1
+//                visited[j] = true
+//
+//                while (queue.isNotEmpty()) {
+//                    val v = queue.removeFirst()
+//
+//                    for (k in 0 until n) {
+//                        if (graph[v][k] == 1) {
+//                            if (visited[k]) continue
+//
+//                            queue.add(k)
+//                            answer[i][k] = 1
+//                            visited[k] = true
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//    answer.forEach {
+//        println(it.joinToString(" "))
+//    }
 }
