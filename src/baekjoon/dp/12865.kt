610b00de2326c9714460bf32bffc9c25e308b95c
dpcks0509 <dpcks0509@gmail.com>
2024-12-22 02:08:36 +0900

    feat: dp - [Gold 5] 12865. 평범한 배낭

---
+++ src/baekjoon/dp/12865.kt
@@ -0,0 +1,27 @@
+package baekjoon.dp
+
+fun main() {
+    val (n, k) = readln().split(" ").map { it.toInt() }
+
+    val w = IntArray(n + 1) { 0 }
+    val v = IntArray(n + 1) { 0 }
+    val dp = Array(n + 1) { IntArray(k + 1) }
+
+    for (i in 1..n) {
+        val (weight, value) = readln().split(" ").map { it.toInt() }
+        w[i] = weight
+        v[i] = value
+    }
+
+    for (i in 1..n) {
+        for (j in 1..k) {
+            if (w[i] > j) {
+                dp[i][j] = dp[i - 1][j]
+            } else {
+                dp[i][j] = maxOf(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+            }
+        }
+    }
+
+    println(dp[n][k])
+}
