d067590d2fab52f62be0bcbd29a3de73caeca274
dpcks0509 <dpcks0509@gmail.com>
2024-12-22 19:18:23 +0900

    feat: tree - [Silver 1] 1991. 트리 순회

--- src/baekjoon/tree/1991.kt
+++ src/baekjoon/tree/1991.kt
@@ -2,24 +2,25 @@ package baekjoon.tree
 
 fun main() {
     data class Node(
-        val value: Char,
+        val value: String,
         var left: Node? = null,
         var right: Node? = null,
     )
 
     val n = readln().toInt()
 
-    val tree = mutableMapOf<Char, Node>()
+    val tree = mutableMapOf<String, Node>()
 
     for (i in 0 until n) {
-        tree['A' + i] = Node('A' + i)
+        val value = ('A' + i).toString()
+        tree[value] = Node(value)
     }
 
     repeat(n) {
-        val (value, left, right) = readln().split(" ").map { it.first() }
-        val currentNode = tree[value]
-        currentNode?.left = if (left != '.') tree[left] else null
-        currentNode?.right = if (right != '.') tree[right] else null
+        val (value, left, right) = readln().split(" ")
+        val node = tree[value]!!
+        node.left = if (left != ".") tree[left] else null
+        node.right = if (right != ".") tree[right] else null
     }
 
     fun preOrder(node: Node?) {
@@ -46,11 +47,11 @@ fun main() {
         print(node.value)
     }
 
-    preOrder(tree['A'])
+    preOrder(tree["A"])
     println()
 
-    inOrder(tree['A'])
+    inOrder(tree["A"])
     println()
 
-    postOrder(tree['A'])
+    postOrder(tree["A"])
 }
