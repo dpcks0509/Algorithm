38dfb1014c0ec54326583deb7280058281833493
dpcks0509 <dpcks0509@gmail.com>
2024-12-22 20:30:08 +0900

    feat: dp - [Gold 5] 9251. LCS

---
+++ src/baekjoon/dp/9251.kt
@@ -0,0 +1,20 @@
+package baekjoon.dp
+
+fun main() {
+    val first = readln()
+    val second = readln()
+
+    val dp = Array(first.length + 1) { IntArray(second.length + 1) }
+
+    for (i in 1..first.length) {
+        for (j in 1..second.length) {
+            if (first[i-1] == second[j-1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = maxOf(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+
+    println(dp[first.length][second.length])
+}
