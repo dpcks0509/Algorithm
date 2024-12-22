54d837c8c8ddc06e193fd7cae27db72998ca4205
dpcks0509 <dpcks0509@gmail.com>
2024-12-22 16:16:47 +0900

    feat: graph - [Silver 2] 11725. 트리의 부모 찾기

---
+++ src/baekjoon/graph/11725.kt
@@ -0,0 +1,41 @@
+package baekjoon.graph
+
+fun main() {
+    val n = readln().toInt()
+
+    val tree = Array(n + 1) { mutableListOf<Int>() }
+    for (i in 0 until n - 1) {
+        val (a, b) = readln().split(" ").map { it.toInt() }
+        tree[a].add(b)
+        tree[b].add(a)
+    }
+
+    val parent = IntArray(n + 1)
+
+    fun bfs() {
+        val queue = ArrayDeque<Int>()
+        val visited = BooleanArray(n + 1)
+
+        queue.add(1)
+        visited[1] = true
+
+        while (queue.isNotEmpty()) {
+            val cur = queue.removeFirst()
+
+            for (i in tree[cur]) {
+                if (visited[i]) continue
+
+                parent[i] = cur
+
+                queue.add(i)
+                visited[i] = true
+            }
+        }
+    }
+
+    bfs()
+
+    for (i in 2..n) {
+        println("${parent[i]}")
+    }
+}
