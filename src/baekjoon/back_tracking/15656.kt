24501c2768a7267b72700f122a051484070bf843
dpcks0509 <dpcks0509@gmail.com>
2025-02-16 16:05:48 +0900

    feat: back_tracking - [Silver 3] 15656. N과 M (7)

---
+++ src/baekjoon/back_tracking/15656.kt
@@ -0,0 +1,24 @@
+package baekjoon.back_tracking
+
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val sb = StringBuilder()
+
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val list = br.readLine().split(" ").map { it.toInt() }.sorted()
+
+    fun recur(depth: Int, str: String) {
+        if (depth == m) {
+            sb.append(str).append("\n")
+            return
+        }
+
+        for (i in 0 until n) {
+            recur(depth + 1, str + "${list[i]} ")
+        }
+    }
+
+    recur(0, "")
+
+    println(sb.toString())
+}
